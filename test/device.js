4364d80caa8ddff9114bfb970b0d3248cf12912b
Patrick Seal <code@plasticrake.com>
2017-09-20 13:01:48 -0700

    test stubs

---
+++ test/device.js
@@ -0,0 +1,8 @@
+/* eslint-env mocha */
+
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+
+const Device = require('..').Device;
---
+++ test/udp-server.js
@@ -0,0 +1,8 @@
+/* eslint-env mocha */
+
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+
+const UpdServer = require('..').UpdServer;
