0d332bc379ae27855e836c241e8128f9c834020b
Patrick Seal <code@plasticrake.com>
2017-09-21 18:18:08 -0700

    remove no longer needed

--- test/utils.js
+++ test/utils.js
@@ -10,20 +10,6 @@ const expect = chai.expect;
 const utils = require('../lib/utils');
 
 describe('utils', function () {
-  // describe('.generateDayList()', function () {
-  //   it('generate day list for given month', function () {
-  //     let dayList = utils.generateDayList(2000, 1);
-  //     expect(dayList).to.have.lengthOf(31);
-  //     expect(dayList).to.have.all.have.keys('year', 'month', 'day');
-  //   });
-  //   it('generate day list for given month with callback data', function () {
-  //     let dayList = utils.generateDayList(2000, 1, () => { return { time: 5 }; });
-  //     expect(dayList).to.have.lengthOf(31);
-  //     expect(dayList).to.have.all.have.keys('year', 'month', 'day', 'time');
-  //     expect(dayList).to.have.all.have.property('time', 5);
-  //   });
-  // });
-
   describe('.getDayList()', function () {
     let dayListData;
     let dayListDataOrig;
@@ -110,24 +96,4 @@ describe('utils', function () {
       expect(data).to.all.have.keys('year', 'month', 'day', 'energy');
     });
   });
-
-  // describe('.mergeDayLists()', function () {
-  //   it('two lists with no common days should be combined', function () {
-  //     let dayListTarget = utils.generateDayList(2000, 1);
-  //     let dayListSource = utils.generateDayList(2001, 1);
-  //     utils.mergeDayLists(dayListTarget, dayListSource);
-  //     expect(dayListTarget).to.have.lengthOf(31 + 31);
-  //     expect(dayListTarget).to.have.all.have.keys('year', 'month', 'day');
-  //   });
-  //
-  //   it('identical lists should stay the same', function () {
-  //     let dayListTarget = utils.generateDayList(2000, 1);
-  //     let dayListSource = utils.generateDayList(2000, 1);
-  //     let originalDaylist = utils.generateDayList(2000, 1);
-  //     utils.mergeDayLists(dayListTarget, dayListSource);
-  //     expect(dayListTarget).to.have.lengthOf(31);
-  //     expect(dayListTarget).to.have.all.have.keys('year', 'month', 'day');
-  //     expect(originalDaylist).to.eql(dayListTarget);
-  //   });
-  // });
 });
