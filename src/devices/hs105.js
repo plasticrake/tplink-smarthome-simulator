20b3f09c442fff0316eb7c5e6070481a5c6e22a1
Patrick Seal <code@plasticrake.com>
2017-10-07 17:19:03 -0700

    hs105 is a weird one

--- src/devices/hs105.js
+++ src/devices/hs105.js
@@ -2,6 +2,8 @@
 
 const merge = require('lodash.merge');
 
+const utils = require('../utils');
+const errCode = utils.errCode;
 const Hs100 = require('./hs100');
 
 const defaultData = require('./data/hs105');
@@ -10,6 +12,10 @@ class Hs105 extends Hs100 {
   constructor (data) {
     super(data);
     merge(this.data, defaultData);
+
+    this.api.system.get_dev_icon = errCode(() => {
+      throw { err_code: -2, err_msg: 'software not support' }; // eslint-disable-line no-throw-literal
+    });
   }
 }
 
