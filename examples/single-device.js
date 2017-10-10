501c5aa4bbb7f54b442d65571e841fd8be4d3d17
Patrick Seal <code@plasticrake.com>
2017-10-09 20:36:03 -0700

    responseDelay example

--- examples/single-device.js
+++ examples/single-device.js
@@ -4,7 +4,8 @@ const Device = require('..').Device;
 const UdpServer = require('..').UdpServer;
 
 // Will generate random mac (used by Kasa app to uniqutely identify a device) and deviceId, etc
-let device = new Device({ model: 'hs100', port: 9999 });
+// add 100ms responseDelay to simulate network latency
+let device = new Device({ model: 'hs100', port: 9999, responseDelay: 100 });
 device.start();
 
 // This uses a hardcoded mac and deviceId.
