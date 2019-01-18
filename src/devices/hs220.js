08cc5649bc0f7de5c2410e5f98f47e2690b7d9e0
Patrick Seal <code@plasticrake.com>
2019-01-18 12:53:30 -0800

    feat: add additional dimmer functions to HS220

--- src/devices/hs220.js
+++ src/devices/hs220.js
@@ -18,10 +18,13 @@ class Hs220 extends Hs200 {
     });
 
     this.api['smartlife.iot.dimmer'] = {
-      set_switch_state: this.api.system.set_relay_state,
 
       set_brightness: errCode(({ brightness }) => {
         this.data.system.sysinfo.brightness = brightness;
+      }),
+
+      get_default_behavior: errCode(() => {
+        return this.data.default_behavior;
       }),
 
       set_dimmer_transition: errCode(({ brightness, mode, duration }) => {
@@ -32,9 +35,31 @@ class Hs220 extends Hs200 {
         return this.data.dimmer_parameters;
       }),
 
-      get_default_behavior: errCode(() => {
-        return this.data.default_behavior;
-      })
+      set_double_click_action: errCode((params) => {
+        this.data.default_behavior.double_click = params;
+      }),
+
+      set_fade_off_time: errCode(({ fadeTime }) => {
+        this.data.dimmer_parameters.fadeOffTime = fadeTime;
+      }),
+
+      set_fade_on_time: errCode(({ fadeTime }) => {
+        this.data.dimmer_parameters.fadeOnTime = fadeTime;
+      }),
+
+      set_gentle_off_time: errCode(({ fadeTime }) => {
+        this.data.dimmer_parameters.gentleOffTime = fadeTime;
+      }),
+
+      set_gentle_on_time: errCode(({ fadeTime }) => {
+        this.data.dimmer_parameters.gentleOnTime = fadeTime;
+      }),
+
+      set_long_press_action: errCode((params) => {
+        this.data.default_behavior.long_press = params;
+      }),
+
+      set_switch_state: this.api.system.set_relay_state
 
     };
   }
