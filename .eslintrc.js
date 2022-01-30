f4602554567058364728ffc2216fc5d6134e606e
Patrick Seal <code@plasticrake.com>
2022-01-30 15:55:32 -0800

    chore: remove babel, add typescript support (#33)
    BREAKING CHANGE: requires minimum node v12.20.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,70 @@
 module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2018,
+    project: ['./tsconfig.json', './tsconfig.eslint.json'],
+  },
   env: {
     browser: false,
     commonjs: true,
     node: true,
   },
-  extends: ['airbnb-base', 'plugin:prettier/recommended'],
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
   reportUnusedDisableDirectives: true,
-  rules: {
-    'no-restricted-syntax': [
-      'off',
-      {
-        selector: 'ForOfStatement',
+
+  ignorePatterns: ['docs'],
+
+  overrides: [
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      extends: [
+        'airbnb-base',
+        'airbnb-typescript/base',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+      ],
+      rules: {
+        '@typescript-eslint/ban-ts-comment': [
+          'error',
+          {
+            'ts-expect-error': 'allow-with-description',
+            'ts-ignore': 'allow-with-description',
+            'ts-nocheck': 'allow-with-description',
+            'ts-check': 'allow-with-description',
+          },
+        ],
+        'no-restricted-syntax': [
+          'off',
+          {
+            selector: 'ForOfStatement',
+          },
+        ],
       },
-    ],
-  },
+    },
+
+    {
+      files: ['*.js'],
+      extends: ['airbnb-base', 'plugin:prettier/recommended'],
+      rules: {
+        'no-restricted-syntax': [
+          'off',
+          {
+            selector: 'ForOfStatement',
+          },
+        ],
+        'import/extensions': 'off',
+        'import/no-unresolved': 'off',
+      },
+    },
+
+    {
+      files: ['examples/*.js', 'examples/*.ts'],
+      rules: {
+        'no-console': 'off',
+        'import/no-unresolved': 'off',
+      },
+    },
+  ],
 };
